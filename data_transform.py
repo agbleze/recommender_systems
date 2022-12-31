ed980a01a3aa1cc6e47924a34f7f99a17fbca951
agbleze <lagbleze@st.ug.edu.gh>
2022-12-31 21:32:40 +0100

    Update data_transform.py

--- data_transform.py
+++ data_transform.py
@@ -4,7 +4,21 @@ from pyspark.sql.functions import col, lit, rand
 
 
 class DataTransformer(object):
+    """Class with methods for preprocessing data using pyspark
+
+    Args:
+        object (_type_): _description_
+    """
     def __init__(self, dataDirpath, useSpark = True, sessionName = 'rs'):
+        """Initializes the DataTransformer class by creating a spark session and
+        loading the data.
+
+        Args:
+            dataDirpath (str): location of data. The data is expected to be a csv file
+            useSpark (_type_): Defaults to True indicating that the spark will be used
+            sessionName (str): Name of the spark session
+        """
+        
         if useSpark:
             self.spark = SparkSession.builder.appName(sessionName).getOrCreate()
             
