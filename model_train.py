b9a954e769c8b9b7b64c6da1c7c173f90e3d5bfe
agbleze <lagbleze@st.ug.edu.gh>
2023-01-10 05:37:20 +0100

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -8,11 +8,6 @@ import seedir
 
 #%%
 
-
-
-
-#%%
-
 seedir.seedir(style='emoji', exclude_folders=['.git', '__pycache__'], 
               exclude_files=['.DS_Store', 'README.md']
               )
@@ -33,11 +28,32 @@ data = transformerPipeline.data
 
 data.show(n=5)
 
+#%%
+data.printSchema()
 #%% the movie title is string and is converted to numberic
 
 indexData = transformerPipeline.convertColumnToNumeric()
 
 indexData.show(n=10)
+
+stringIndexerModel = transformerPipeline.getStringIndexer()
+
+
+stringIndexPath = get_file_path(folder_name='model_store', file_name='string-indexer-model')
+
+
+#%%
+stringIndexerModel.write().overwrite().save(path=stringIndexPath)
+
+#%%
+
+
+# indexData.write.format('csv')\
+#     .mode('overwrite')\
+#         .options(sep=',', header='true').save('indexed_data.csv')
+
+
+
 
 
 #%% split data
@@ -48,15 +64,51 @@ trainData, testData = transformerPipeline.splitData()
 # %%
 alsModel = AlternateLeastSquaresModel(userCol='userId',
                                       itemCol='title_new', 
-                                      ratingCol='rating_new', 
+                                      ratingCol='rating', 
                                       trainData=trainData,
                                       testData=testData
                                       )
 
 #%%
 
-alsModel.fit()
+fittedData = alsModel.fit()
 
+
+#%%
+predictionData = alsModel.predict()
+
+
+#%%
+alsModel.evaluateModel()
+
+
+#%%
+alsModel.saveModel(modelStoreFolderName='model_store')
+
+# %%
+loadedModel = alsModel.getModel(modelStoreFolderName='model_store')
+
+
+#%%
+
+from helper import recommendItem
+
+#%%
+
+userRecommendedItems = recommendItem(userId= 100, modelStoreFolderName='model_store',
+                                    modelName='model.h5',
+                                    numberOfItems=5, 
+                                    getModel=AlternateLeastSquaresModel.getModel,
+                                    readIndexedDataFromFile=False,
+                                    indexedDataObject=indexData,
+                                    stringIndexDir=get_file_path(folder_name='model_store', 
+                                                                file_name='string-indexer-model'
+                                                                )
+                                    )
+
+
+#%%
+userRecommendedItems.select('title').show()
 
 
 
