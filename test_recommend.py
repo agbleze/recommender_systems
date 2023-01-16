b1ebdeaeec2ecbc5ab4539d3d47921c61251f1f7
agbleze <lagbleze@st.ug.edu.gh>
2023-01-16 02:32:18 +0100

    Update test_recommend.py

--- test_recommend.py
+++ test_recommend.py
@@ -14,21 +14,30 @@ data = dt.data
 
 
 @pytest.fixture()
-def create_model_instance():
-    model = AlternateLeastSquaresModel()
-    return model
-
-@pytest.fixture()
-def create_train_test_data():
-    pass
-
-
-@pytest.fixture()
-def create_data_transformer_object():
+def data_transformer():
     dt = DataTransformer(dataDirpath=data_file_path, sessionName='recommender')
     return dt
 
 
+@pytest.fixture()
+def train_test_data(data_transformer):
+    data_transformer.convertColumnToNumeric()
+    train_data, test_data = data_transformer.splitData()
+    return train_data, test_data
+
+
+@pytest.fixture()
+def ALS_model(train_test_data):
+    train_data, test_data = train_test_data
+    model = AlternateLeastSquaresModel(trainData=train_data, testData=test_data)
+    return model
+
+
+@pytest.fixture()
+def prediction(ALS_model):
+    ALS_model.fit()
+    prediction = ALS_model.predict()
+    return prediction
 
 
 def test_title_column_is_availaible():
@@ -36,49 +45,34 @@ def test_title_column_is_availaible():
 
 
 
-def test_title_column_is_numeric():
-    pass
+# def test_title_column_is_numeric():
+#     pass
 
 
+def test_length_data_split(train_test_data):
+    assert len(train_test_data) == 2  # indicates both train and test data were returned successfully
 
 
-def test_shape_spitData():
-    dt.convertColumnToNumeric()
-    assert len(dt.splitData()) == 2  # indicates both train and test data were returned successfully
-
-
-
-def test_get_string_indexer_type():
-    assert type(dt.getStringIndexer()) == pyspark.ml.feature.StringIndexerModel
+def test_get_string_indexer_type(data_transformer):
+    data_transformer.convertColumnToNumeric()
+    assert isinstance(data_transformer.getStringIndexer(), 
+                      pyspark.ml.feature.StringIndexerModel
+                      )
     
 
+def test_fit_model(ALS_model):
+    assert isinstance(ALS_model.fit(), pyspark.ml.recommendation.ALSModel)    
+
+def test_prediction_object_type(prediction):
+    assert isinstance(prediction, pyspark.sql.dataframe.DataFrame)
+
+def test_prediction_available(prediction):
+    assert 'prediction' in prediction.columns 
 
 
-def test_fit_model():
-    pass    
-
-def test_predict():
-    pass
-
-
-
-def testing_convertColumnToNumeric():
-    pass
-
-
-
-def test_splitData():
-    pass
-
-
-
-def test_get_model():
-    pass
-
-
-
-def test_fit_model():
-    pass
+def test_get_model(ALS_model):
+    loaded_model = ALS_model.getModel(modelStoreFolderName='model_store')
+    assert isinstance(loaded_model, pyspark.ml.recommendation.ALSModel)
 
 
 def test_predict_ratings():
